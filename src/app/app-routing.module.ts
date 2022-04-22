451950d33e7ec159c56d11b7dc52caf080a6a5c3
ssalazaruandina <019101931k@uandina.edu.pe>
2022-04-22 01:11:45 -0700

    sidebar mejorado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Componentes/login/login.component';
+import { PerfilComponent } from './Componentes/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'perfil',
+    component: PerfilComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './Componentes/side-bar/side-bar.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { PerfilComponent } from './Componentes/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    LoginComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
